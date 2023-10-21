c84af642e17e45b1cdbaf97efe413931fb55cb69
noname44k1 <thanh.cntt.dhv@gmail.com>
2023-10-21 11:23:25 +0700

    21/10/2023 - code blog

---
+++ PTUDW_01_KHMT/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTUDW_01_KHMT.Models;
+
+namespace PTUDW_01_KHMT.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly HarmicContext _context;
+
+        public BlogController(HarmicContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var blogs = _context.TbBlogs.Where(i => (bool)i.IsActive).OrderByDescending(i => i.BlogId).ToList();
+            ViewBag.blogComment = _context.TbBlogComments.Where(m => (bool)m.IsActive).ToList();
+            return View(blogs);
+        }
+
+        [Route("/blog/{alias}-{id}.html")]
+        public IActionResult Details(int? id) {
+            if (id == null)
+                return NotFound();
+            var blog = _context.TbBlogs.FirstOrDefault(i => i.BlogId == id && (bool)i.IsActive);
+            if (blog == null)
+                return NotFound();
+            return View(blog);
+        }
+    }
+}
